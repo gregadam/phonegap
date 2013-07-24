67a2345df24839dca67373ff1730c1a779cf9ad7
Greg Adam <gregadam@gmail.com>
2013-07-24 14:58:56 +0100

    Add video

--- app.js
+++ app.js
@@ -18,6 +18,27 @@ function onFail(message) {
 }
 
 /*
+	Capture video
+*/
+
+function captureVideoSuccess(mediaFiles) {
+    var i, path, len;
+    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+        path = mediaFiles[i].fullPath;
+        // do something interesting with the file
+    }
+};
+
+// capture error callback
+function captureVideoError(error) {
+    navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
+};
+
+function captureVideo() {
+	// start audio capture
+	navigator.device.capture.captureVideo(captureVideoSuccess, captureVideoError, {limit:2});
+}
+/*
 	Capture audio
 */
 
