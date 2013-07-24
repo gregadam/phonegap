07f276ffd99001bc351602ad8358758cd6bdb464
Greg Adam <gregadam@gmail.com>
2013-07-24 13:27:21 +0100

    Fix

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ function captureError(error) {
 
 function captureAudio() {
 	// start audio capture
-	if (alert("Starting audio capture: " + navigator.audio);
+	alert("Starting audio capture: " + navigator.audio)
 	navigator.device.capture.captureAudio(captureSuccess, captureError, {limit:2});
 }
 
