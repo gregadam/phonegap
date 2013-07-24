5706393913e874de34d176e4aff97f148e5e16b3
Greg Adam <gregadam@gmail.com>
2013-07-24 13:16:06 +0100

    Add initial audio stuff.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/*
+	Capture Photo
+*/
+
 function capturePhoto() {  
     navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
         destinationType: Camera.DestinationType.DATA_URL
@@ -13,4 +17,25 @@ function onFail(message) {
     alert('Failed because: ' + message);
 }
 
+/*
+	Capture audio
+*/
+
+function captureSuccess(mediaFiles) {
+    var i, path, len;
+    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+        path = mediaFiles[i].fullPath;
+        // do something interesting with the file
+    }
+};
+
+// capture error callback
+function captureError(error) {
+    navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
+};
+
+function captureAudio() {
+	// start audio capture
+	navigator.device.capture.captureAudio(captureSuccess, captureError, {limit:2});
+}
 
